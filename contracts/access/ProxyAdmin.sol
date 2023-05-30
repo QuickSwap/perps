95f794918b5217b942ba784e2ea2046d26b67bbb
0xMattAxel <>
2023-05-30 12:04:12 +0300

    ProxyAdmin updated & Farming removed & rewardrouter rolled back

--- contracts/access/ProxyAdmin.sol
+++ contracts/access/ProxyAdmin.sol
@@ -4,8 +4,10 @@ pragma solidity  0.8.18;
 
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract ProxyAdmin is AccessControl {
+    using SafeERC20 for IERC20;
 
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
@@ -16,11 +18,12 @@ contract ProxyAdmin is AccessControl {
         bytes data
     );
 
+    event WithdrawToken(address indexed sender, address receiver, address token, uint256 amount);
+
     constructor(address admin) {
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
         _grantRole(ADMIN_ROLE, admin);
     }
-
 
     function functionCallWithValue(
         address target,
@@ -38,6 +41,17 @@ contract ProxyAdmin is AccessControl {
             value,
             data
         );
+    }
+
+    function withdrawToken(
+        IERC20 token,
+        address receiver
+    ) external onlyRole(ADMIN_ROLE) 
+    {
+        uint256 balance = token.balanceOf(address(this));
+        require(balance>0,"Not enough balance");
+        token.safeTransfer(receiver, balance);
+        emit WithdrawToken(msg.sender, receiver, address(token), balance);
     }
 
 }
